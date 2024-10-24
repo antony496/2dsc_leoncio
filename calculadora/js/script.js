ee9e3cacbd246d5a24ea475208792a2ad3cefc66
antony496 <t691411@gmail.com>
2024-10-24 14:55:55 +0000

    finalização

--- calculadora/js/script.js
+++ calculadora/js/script.js
@@ -1,32 +1,34 @@
 // FAZ REFERÊNCIA AOS ELEMENTOS DOM
 
+
+
 const numero1 = document.querySelector('#numero1')
 const numero2 = document.querySelector('#numero2')
-const btnCalcular = document.querySelector('#btn-calcular')
-const resultado = document.querySelector('.resultado')
-const adicao = document.querySelector('.adicao')
-const subtracao = document.querySelector('.subtracao')
-const multiplicacao = document.querySelector('.multiplicacao')
-const divisao = document.querySelector('.divisao')
-
-//alert(numero1)
-// CRIA A FUNÇÃO CALCULAR
-function calcular(){
-    const n1 = Number(numero1.value)
-    const n2 = Number(numero2.value)
-    if(typeof n1 === 'number'
-        && typeof n2 === 'number' ) {
-          const adicao =  `A soma de ${n1} e ${n2} = ${n1+n2}`
-          alert(add)
-          adicao.innerHTML = add
-        } else {
-            alert('Por favor, digite um número correto')
-        }
+ const btncalcular = document.querySelector('#btn-calcular') 
+ const resultado = document.querySelector('.resultado')
+  const divisao = document.querySelector('.divisao') 
+  const adicao = document.querySelector('.adicao')
+   const subtracao = document.querySelector('.subtracao')
+    const multiplicacao = document.querySelector('.multiplicacao') 
    
-    //alert('Resultado: ' + resultado)
+    function calcular(){ 
+    const n1 = Number(numero1.value) 
+    const n2 = Number(numero2.value) 
+    if(typeof n1 === 'number' && typeof n2 === 'number')
+         { const add = `O resultado da Adição é: ${n1+n2}` 
+    const div = `O resultado da Divisão é: ${n1/n2}` 
+    const sub = `O resultado da Subtração é: ${n1-n2}`
+     const mult = `O resultado da Multiplicação é: ${n1*n2}` 
+     //alert(adicao) 
+     adicao.innerHTML = add 
+     divisao.innerHTML = div
+     subtracao.innerHTML = sub 
+     multiplicacao.innerHTML = mult 
+    } 
+    else { alert('por favor, digite um numero correto') 
+
+    } 
 }
-// ADICIONA ESCUTAR DE EVENTOS
-btnCalcular.addEventListener('click', function(evento){
-    evento.preventDeFault() // TIRAR O COMPORTAMENTO PADRÃO
-    calcular()
-})
+btncalcular.addEventListener('click', function(evento){ 
+    evento.preventDefault() 
+    calcular() })
